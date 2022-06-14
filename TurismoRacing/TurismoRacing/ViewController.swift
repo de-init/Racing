4249629e694178128171b34efa748967684c2dbd
DD <snawyx@gmail.com>
2022-06-14 13:53:53 +0300

    Add project

---
+++ TurismoRacing/TurismoRacing/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TurismoRacing
+//
+//  Created by S2 on 14.06.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
